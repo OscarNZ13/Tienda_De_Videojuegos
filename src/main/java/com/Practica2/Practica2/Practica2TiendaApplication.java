2918b67c647e640ca0f25f283b61dc854d174781
OscarNZ13 <133930637+OscarNZ13@users.noreply.github.com>
2023-06-02 14:02:34 -0600

    Se creo el proyecto para la segunda practica y vinculado con el repositorio de github

---
+++ src/main/java/com/Practica2/Practica2/Practica2TiendaApplication.java
@@ -0,0 +1,13 @@
+package com.Practica2.Practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2TiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2TiendaApplication.class, args);
+	}
+
+}
